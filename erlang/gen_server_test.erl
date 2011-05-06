476d70ad0b37db3408dfcc4c350df162a46237c0
sharp <sharp@sharp-ThinkPad-SL400.(none)>
2011-05-06 18:31:09 +0800

    fix gen server test

--- erlang/gen_server_test.erl
+++ erlang/gen_server_test.erl
@@ -1,26 +1,29 @@
 -module(gen_server_test).
 -behaviour(gen_server).
--export([start/0, start_link/0]).
--export([alloc/0, alloc/1, free/1]).
+-export([start_link/0]).
+-export([alloc/0, free/1]).
 -export([init/1, handle_call/3, handle_cast/2]).
-start() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).  
-stop()  -> gen_server:call(?MODULE, stop). 
 start_link() ->
     gen_server:start_link({local, gen_server_test}, gen_server_test, [], []).
+
 alloc() ->
     io:format("allocing"),
-		gen_server:call(gen_server_test, alloc).
+		gen_server:call(?MODULE, {alloc}).
+
 free(Ch) ->
     gen_server:cast(gen_server_test, {free, Ch}).
+
 init(_Args) ->
     {ok, channels()}.
-handle_call(alloc, _From, Chs) ->
+
+handle_call({alloc}, _From, LoopData) ->
     io:format("handle alloc").
-andle_cast({free, Ch}, Chs) ->
+
+handle_cast({free, Ch}, LoopData) ->
     io:format(Ch).
+
 free() -> 
 		io:format("free").
+
 channels() ->
 		io:format("channels").
-alloc(chs) -> 
-		io:format("alloc chs").
